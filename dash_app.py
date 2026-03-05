945c11ee3db0aa9963d4636660b70d200ba50dc2
eche809 <132237840+ec0207@users.noreply.github.com>
2026-03-05 23:27:36 +1100

    task3 complete

---
+++ dash_app.py
@@ -0,0 +1,49 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+
+from dash import Dash, html, dcc,  Input, Output
+import plotly.express as px
+import pandas as pd
+
+app = Dash()
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv('./output_sales_data.csv')
+
+fig = px.line(df, x="date", y="sales", line_group='region')
+
+app.layout = html.Div(children=[
+    html.H1(children='Pink Morsel Sales'),
+    dcc.Dropdown(
+        id = 'region-dropdown',
+        options = [{"label": r, "value": r} for r in df["region"].unique()],
+        value = df["region"].unique()[0]
+    ),
+    dcc.Graph(
+        id='sales-graph',
+        figure=fig
+    )
+])
+
+@app.callback(
+    Output("sales-graph", "figure"),
+    Input("region-dropdown", "value")
+)
+def update_graph(selected_region):
+
+    filtered = df[df["region"] == selected_region]
+
+    fig = px.line(
+        filtered,
+        x="date",
+        y="sales",
+        title=f"Sales in {selected_region}"
+    )
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

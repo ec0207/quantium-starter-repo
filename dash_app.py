24e58d4583d26f8a28203dbefb131b3a8e608585
eche809 <132237840+ec0207@users.noreply.github.com>
2026-03-07 19:37:45 +1100

    simple color changes

--- dash_app.py
+++ dash_app.py
@@ -12,14 +12,34 @@ app = Dash()
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.read_csv('./output_sales_data.csv')
 
-fig = px.line(df, x="date", y="sales", line_group='region')
+colors = {
+    'background': "#4745D9",
+    'text': '#45D7D9'
+}
 
-app.layout = html.Div(children=[
-    html.H1(children='Pink Morsel Sales'),
-    dcc.Dropdown(
-        id = 'region-dropdown',
-        options = [{"label": r, "value": r} for r in df["region"].unique()],
-        value = df["region"].unique()[0]
+fig = px.line(df, x="date", y="sales", line_group='region')
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text'],
+    title_x=0.5
+)
+
+app.layout = html.Div(
+    children=[
+    html.H1(children='Pink Morsel Sales', 
+            style={'textAlign': "center", 
+                    "color": "#D9458D",
+                    "background": colors["background"]}),
+    html.H2(children="Select Sales in region:", 
+           style={"color": colors["text"]}),
+    dcc.RadioItems(
+        id="region-radio",
+        options=[
+            {"label": r, "value": r} for r in df["region"].unique() 
+        ] + [{"label": "All Regions", "value": "ALL"}],
+        value=df["region"].unique()[0],
+        inline=True   # display buttons horizontally
     ),
     dcc.Graph(
         id='sales-graph',
@@ -29,18 +49,22 @@ app.layout = html.Div(children=[
 
 @app.callback(
     Output("sales-graph", "figure"),
-    Input("region-dropdown", "value")
+    Input("region-radio", "value")
 )
 def update_graph(selected_region):
-
-    filtered = df[df["region"] == selected_region]
+    
+    if selected_region == "ALL":
+        filtered = df
+        title = "Sales in All Regions"
+    else: 
+        filtered = df[df["region"] == selected_region]
+        title =  f"Sales in {selected_region}"
 
     fig = px.line(
         filtered,
         x="date",
         y="sales",
-        title=f"Sales in {selected_region}"
-    )
+        title=title)
 
     return fig
 
